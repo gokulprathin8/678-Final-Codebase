fe1f3373a14587f37cf502ad1cbad49a377af811
Gokul Prathin <gokulprathin8@gmail.com>
2023-11-28 02:56:30 -0500

    added remove, replace and add neutral function

---
+++ robustness/remove_replace_add_neutral.py
@@ -0,0 +1,32 @@
+import random
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+
+def auto_word_modification(sentence: str, threshold=0.2):
+  stop_words = set(stopwords.words('english'))
+  neutral_words = ["something", "thing", "area", "case", "day"]  # Example list of neutral words
+  words = sentence.split()
+  modified = []
+
+  for word in words:
+    if random.random() < threshold:
+      if word.lower() not in stop_words:
+        modified.append(random.choice(neutral_words) if random.random() < 0.5 else word)
+    else:
+      if word.lower() not in stop_words:
+        modified.append(word)
+
+  if random.random() < threshold:
+    # Add a random neutral word
+    modified.append(random.choice(neutral_words))
+
+  return ' '.join(modified)
+
+
+if __name__ == '__main__':
+  sentence = "This is a sample sentence with a few common words"
+  modified_sentence = auto_word_modification(sentence, threshold=0.2)
+  print(modified_sentence)
